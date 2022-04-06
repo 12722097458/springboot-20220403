db4707e6a07d71842137493a1652efd8fd27f736
Jack <ayinjun1109@163.com>
2022-04-06 23:34:00 +0800

    The use of @AutoConfiguration in SpringBoot

--- src/main/java/com/ityj/boot/BootApplication.java
+++ src/main/java/com/ityj/boot/BootApplication.java
@@ -1,13 +1,29 @@
 package com.ityj.boot;
 
+import com.ityj.boot.config.MyConfig;
+import com.ityj.boot.entity.Pet;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
 
 @SpringBootApplication
 public class BootApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(BootApplication.class, args);
+		// 返回IOC容器
+		ConfigurableApplicationContext run = SpringApplication.run(BootApplication.class, args);
+		// 查看容器里的所有组件
+		String[] beanDefinitionNames = run.getBeanDefinitionNames();
+		for (String name : beanDefinitionNames) {
+			System.out.println(name);
+		}
+
+		MyConfig myConfig = run.getBean(MyConfig.class);
+		System.out.println("myConfig = " + myConfig);
+
+		Pet myPet = run.getBean("myPet", Pet.class);
+		System.out.println("myPet = " + myPet);
+
 	}
 
 }
---
+++ src/main/java/com/ityj/boot/config/MyConfig.java
@@ -0,0 +1,23 @@
+package com.ityj.boot.config;
+
+import com.ityj.boot.entity.Pet;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ *  1、配置类里面使用标注在方法上给容器注册组件，默认是单实例的
+ *  2、配置类MyConfig本身也是一个组件
+ *  3、在spring5.2后@Configuration里有一个proxyBeanMethods属性，可以控制方法是否通过代理生成，
+ *      默认为true，Full通过代理生成com.ityj.boot.config.MyConfig$$EnhancerBySpringCGLIB$$146ac44c@aca3c85
+ *      false -> Lite: 每次new一个对象
+  */
+@Configuration   // 告诉SpringBoot这是一个配置类 == 配置文件的作用
+public class MyConfig {
+
+    // 默认单实例
+    @Bean  // 给容器中添加组件，以方法名作为组件的id；返回类型就是组件类型；返回的值就是组件在容器中的实例
+    public Pet myPet() {
+        return new Pet("tomcat");
+    }
+
+}
