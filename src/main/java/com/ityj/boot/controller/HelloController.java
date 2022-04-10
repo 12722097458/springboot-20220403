cd6608f7c786ce9fadc691e7b3076a6de52bca83
Jack <ayinjun1109@163.com>
2022-04-10 13:47:43 +0800

    how to config list in application.yml data:  list: Jack,Rose,Tom 1. SpringBoot默认以逗号将值隔开，可以用@value直接获取（arr,list都可以） 2. 用表达式@Value("#{'${data.list}'.split(',')}") 其中list或arr接收都可以

--- src/main/java/com/ityj/boot/controller/HelloController.java
+++ src/main/java/com/ityj/boot/controller/HelloController.java
@@ -3,8 +3,12 @@ package com.ityj.boot.controller;
 import com.ityj.boot.entity.Car;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Arrays;
+import java.util.List;
 
 @RestController
 @Slf4j
@@ -13,9 +17,38 @@ public class HelloController {
     @Autowired
     private Car car;
 
+    @Value("${data.str}")
+    private String str;
+
+    @Value("${data.number}")
+    private double num;
+
+    @Value("${data.boolean}")
+    private boolean flag;
+
+    @Value("${data.list}")
+    private List<String> list;  // Spring默认情况下会以','进行分割，转换成对应的数组或List。
+    @Value("${data.list}")
+    private String[] arr;
+
+    @Value("#{'${data.list}'.split(',')}")   // 数组或list接收都可以
+    private List<String> list2;
+    @Value("#{'${data.list}'.split(',')}")
+    private String[] arr2;
+
     @GetMapping(path = "/hello")
     public String sayHello() {
         log.info(car.toString());
         return "Hello Spring Boot!";
+    }
+
+    @GetMapping(path = "/data")
+    public String getData() {
+        log.info("str = {}", str);
+        log.info("num = {}", num);
+        log.info("flag = {}", flag);
+        log.info("list = {}", list);
+        log.info("arr = {}", Arrays.toString(arr));
+        return "Success";
     }
 }
